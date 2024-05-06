5ad93e93f53902be3879e6d559c766ad597b49fa
MrDendy <adim.azizov0@gmail.com>
2024-05-06 10:55:28 +0300

    save

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Liba;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]//такого дерьма должно быть ~10
+        public void TestMethod1()
+        {
+            int expResult = 0;
+            int actResult = Balvan.Add(-1, 1);
+            Assert.AreEqual(expResult, actResult, "Тест1");
+        }
+    }
+}
